e50ff65260279248f837dcd875d5dcfb2cfc120f
King Jack <student@jmoran2.local>
2016-12-18 14:56:30 -0600

    Initial Commit

---
+++ Ohm's Law CalculatorTests/Ohm_s_Law_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ohm_s_Law_CalculatorTests.swift
+//  Ohm's Law CalculatorTests
+//
+//  Created by jmoran2 on 12/18/16.
+//  Copyright © 2016 Big J's Kobe size development team. All rights reserved.
+//
+
+import XCTest
+@testable import Ohm_s_Law_Calculator
+
+class Ohm_s_Law_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
